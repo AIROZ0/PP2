287a68010f7781f57210585f6860b76a790f34ea
master <askarbek70@inbox.ru>
2025-01-31 21:23:47 +0500

    lab1 done

---
+++ lab1/Data Type/All.py
@@ -0,0 +1,14 @@
+x = 5
+print(type(x)) 
+
+print(type("Hello World!"))
+
+print(type(0.5))
+print(type([i for i in range(5)]))
+print(type({i : i for i in range(5)}))
+print(type({i for i in range(5)}))
+print(type(frozenset({i for i in range(5)})))
+print(type(5>2))
+print(type(5<2))
+print(type(b"Hello"))
+print(type(bytearray(5)))
---
+++ lab1/Syntax/tabul.py
@@ -0,0 +1,13 @@
+if 5>2:
+    print("Five is greater than two!")
+
+#This is a comment.
+print("Hello, World!")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+if 5 > 2:
+ print("Five is greater than two!")
